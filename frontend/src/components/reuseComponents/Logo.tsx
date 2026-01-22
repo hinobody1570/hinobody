59147a4025cbfed230ffb29c8ce8e79b829b9ec2
Asad-dev662 <132562232+Ironmandeveloper@users.noreply.github.com>
2026-01-22 16:42:24 +0500

    make minor logo responsive

--- frontend/src/components/reuseComponents/Logo.tsx
+++ frontend/src/components/reuseComponents/Logo.tsx
@@ -3,11 +3,11 @@ import LOGO_ICON from "../../../public/assets/images/logo/logo-icon.png";
 import LOGO_TEXT from "../../../public/assets/images/logo/logo-text.png";
 
 interface LogoType {
-  text: string;
+  text?: string;
   onClick: () => void;
 }
 
-export const Logo = ({ text = "reddit", onClick }: LogoType) => {
+export const Logo = ({ onClick }: LogoType) => {
   return (
     <button onClick={onClick} className="w-28 sm:w-32 md:w-40 cursor-pointer flex items-center gap-1 hover:opacity-80 transition-opacity">
       <Image src={LOGO_ICON} alt="LOGO_ICON" className="w-8 sm:w-10 md:w-12 object-fill" />
